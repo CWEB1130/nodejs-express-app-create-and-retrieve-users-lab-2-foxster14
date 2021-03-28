7e96d3f219444b1ca0823a2c4889977783c655d0
sarahfox <sarah.fox.h@gmail.com>
2021-03-27 23:00:54 -0500

    added name comment to app.js & finished steps 6-7

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//Sarah Fox//
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
--- routes/createboxer.js
+++ routes/createboxer.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+//the statement below contains a render function - the first argument is the view name and the second argument is an object with one key/value pair.
+res.render('createboxer', { title: 'Create Boxer'})
 
 module.exports = router;
